d35b0f8bb41471a7cf403bda55aafeb4528453b0
Nithish Kolli <nithish.1993@gmail.com>
2024-12-19 12:36:54 -0800

    Day 7

---
+++ AdventOfCode/2024/Day7.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Day7 {
+    public static void main(String[] args) {
+        // String filePath = "day7input.txt";
+        Path filePath  = Paths.get("day7inputsample.txt");
+        try {
+            List<String> lines = Files.readAllLines(filePath);
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
+    }
+}
