a431942f0b4357e6125a76785ee27d90dd41ebf2
Nithish Kolli <nithish.1993@gmail.com>
2024-12-19 14:52:52 -0800

    Day 8 complete

--- AdventOfCode/2024/Day8.java
+++ AdventOfCode/2024/Day8.java
@@ -16,6 +16,7 @@ public class Day8 {
         // Path filePath = Paths.get("day8inputsample.txt");
         Map<String, List<Map.Entry<Integer, Integer>>> map = new HashMap<>();
         Set<Map.Entry<Integer, Integer>> set = new HashSet<>();
+        Set<Map.Entry<Integer, Integer>> set2 = new HashSet<>();
         int rowCounter = 0;
         int columnSize = 0;
         try {
@@ -38,14 +39,16 @@ public class Day8 {
         for (String key : map.keySet()) {
             List<Map.Entry<Integer, Integer>> entryList = map.get(key);
             for (int i = 0; i < entryList.size(); i++) {
-                for (int j = i + 1; j < entryList.size(); j++) {
+                for (int j = i + 1 ; j < entryList.size(); j++) {
                     if (!entryList.get(i).equals(entryList.get(j))) {
                         addAntiNodesForPair(entryList.get(i), entryList.get(j), rowCounter, columnSize, set);
+                        addAntiNodesForPairUpdated(entryList.get(i), entryList.get(j), rowCounter, columnSize, set2);
                     }
                 }
             }
         }
         System.out.println(set.size());
+        System.out.println(set2.size());
     }
 
     private static void addAntiNodesForPair(Map.Entry<Integer, Integer> pair1, Map.Entry<Integer, Integer> pair2,
@@ -60,6 +63,28 @@ public class Day8 {
         int secY = pair2.getValue()+(pair2.getValue()-pair1.getValue());
         if(0<=secX && secX<rowSize && secY>=0 && secY<columnSize){
             set.add(new AbstractMap.SimpleEntry<Integer,Integer>(secX,secY));
+        }
+    }
+
+    private static void addAntiNodesForPairUpdated(Map.Entry<Integer, Integer> pair1, Map.Entry<Integer, Integer> pair2,
+            int rowSize, int columnSize, Set<Map.Entry<Integer, Integer>> set) {
+        int i=0;
+        int firstX = pair1.getKey()+(pair1.getKey()-pair2.getKey())*i;
+        int firstY = pair1.getValue()+(pair1.getValue()-pair2.getValue())*i;
+        while(0<=firstX && firstX<rowSize && firstY>=0 && firstY<columnSize){
+            set.add(new AbstractMap.SimpleEntry<Integer,Integer>(firstX,firstY));
+            i++;
+            firstX = pair1.getKey()+(pair1.getKey()-pair2.getKey())*i;
+            firstY = pair1.getValue()+(pair1.getValue()-pair2.getValue())*i;
+        }
+        i=0;
+        int secX = pair2.getKey()+(pair2.getKey()-pair1.getKey())*i;
+        int secY = pair2.getValue()+(pair2.getValue()-pair1.getValue())*i;
+        while(0<=secX && secX<rowSize && secY>=0 && secY<columnSize){
+            set.add(new AbstractMap.SimpleEntry<Integer,Integer>(secX,secY));
+            i++;
+            secX = pair2.getKey()+(pair2.getKey()-pair1.getKey())*i;
+            secY = pair2.getValue()+(pair2.getValue()-pair1.getValue())*i;
         }
     }
 }
