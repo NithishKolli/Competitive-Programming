b44fb16d3fcb47d4fad9f6d2706ab2cea65502a1
Nithish Kolli <nithish.1993@gmail.com>
2024-12-19 14:36:03 -0800

    Day8 part 1

--- AdventOfCode/2024/Day8.java
+++ AdventOfCode/2024/Day8.java
@@ -2,19 +2,64 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Day8 {
     public static void main(String[] args) {
-        // Path filePath  = Paths.get("day8input.txt");
-        Path filePath  = Paths.get("day8inputsample.txt");
+        Path filePath = Paths.get("day8input.txt");
+        // Path filePath = Paths.get("day8inputsample.txt");
+        Map<String, List<Map.Entry<Integer, Integer>>> map = new HashMap<>();
+        Set<Map.Entry<Integer, Integer>> set = new HashSet<>();
+        int rowCounter = 0;
+        int columnSize = 0;
         try {
+
             List<String> lines = Files.readAllLines(filePath);
             for (String line : lines) {
-                System.out.println(line);
+                String[] parts = line.split("");
+                for (int i = 0; i < parts.length; i++) {
+                    if (!parts[i].equals(".")) {
+                        map.computeIfAbsent(parts[i], k -> new ArrayList<Map.Entry<Integer, Integer>>())
+                                .add(new AbstractMap.SimpleEntry<>(rowCounter, i));
+                    }
+                }
+                rowCounter++;
             }
+            columnSize = lines.get(0).length();
         } catch (IOException e) {
             System.out.println("Error reading the file: " + e.getMessage());
+        }
+        for (String key : map.keySet()) {
+            List<Map.Entry<Integer, Integer>> entryList = map.get(key);
+            for (int i = 0; i < entryList.size(); i++) {
+                for (int j = i + 1; j < entryList.size(); j++) {
+                    if (!entryList.get(i).equals(entryList.get(j))) {
+                        addAntiNodesForPair(entryList.get(i), entryList.get(j), rowCounter, columnSize, set);
+                    }
+                }
+            }
+        }
+        System.out.println(set.size());
+    }
+
+    private static void addAntiNodesForPair(Map.Entry<Integer, Integer> pair1, Map.Entry<Integer, Integer> pair2,
+            int rowSize, int columnSize, Set<Map.Entry<Integer, Integer>> set) {
+        
+        int firstX = pair1.getKey()+(pair1.getKey()-pair2.getKey());
+        int firstY = pair1.getValue()+(pair1.getValue()-pair2.getValue());
+        if(0<=firstX && firstX<rowSize && firstY>=0 && firstY<columnSize){
+            set.add(new AbstractMap.SimpleEntry<Integer,Integer>(firstX,firstY));
+        }
+        int secX = pair2.getKey()+(pair2.getKey()-pair1.getKey());
+        int secY = pair2.getValue()+(pair2.getValue()-pair1.getValue());
+        if(0<=secX && secX<rowSize && secY>=0 && secY<columnSize){
+            set.add(new AbstractMap.SimpleEntry<Integer,Integer>(secX,secY));
         }
     }
 }
