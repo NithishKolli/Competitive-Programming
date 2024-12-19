90d02cfae8e1beebfa9cc75db265d756ed076e14
Nithish Kolli <nithish.1993@gmail.com>
2024-12-19 15:49:22 -0800

    Day 9 part 1

--- AdventOfCode/2024/Day9.java
+++ AdventOfCode/2024/Day9.java
@@ -6,15 +6,48 @@ import java.util.List;
 
 public class Day9 {
     public static void main(String[] args) {
-        // Path filePath  = Paths.get("day9input.txt");
-        Path filePath  = Paths.get("day9inputsample.txt");
+        Path filePath  = Paths.get("day9input.txt");
+        // Path filePath  = Paths.get("day9inputsample.txt"); 
+        long answer = 0;
         try {
             List<String> lines = Files.readAllLines(filePath);
             for (String line : lines) {
-                System.out.println(line);
+                int finalArrIndex = 0;
+                int leftPointer = 0;
+                int rightPointer = line.length()-1;
+                int leftIndex = 0;
+                int rightIndex = (line.length())/2;
+                int[] parts = line.chars()
+                                    .map(c -> c - '0')
+                                    .toArray();;
+                while(leftPointer<line.length()-1 && rightPointer>0){
+                    if(leftPointer%2==0) { // even, so integ
+                        if(parts[leftPointer] ==  0){
+                            leftPointer++;
+                        } else {
+                            answer+= finalArrIndex*leftIndex;
+                            parts[leftPointer]--;
+                            finalArrIndex++;
+                        }
+                    } else {
+                        if(parts[leftPointer] ==  0){ //
+                            leftPointer++;
+                            leftIndex++;
+                        } else if(parts[rightPointer]==0) {
+                            rightPointer -= 2;
+                            rightIndex--;
+                        } else {
+                            parts[leftPointer]--;
+                            parts[rightPointer]--;
+                            answer += finalArrIndex*rightIndex;
+                            finalArrIndex++;
+                        }
+                    }
+                }
             }
         } catch (IOException e) {
             System.out.println("Error reading the file: " + e.getMessage());
         }
+        System.out.println(answer);
     }
 }
