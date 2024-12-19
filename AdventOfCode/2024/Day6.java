5bcb1f7cb89773aab60220c641e8063d128645d6
Nithish Kolli <nithish.1993@gmail.com>
2024-12-19 12:28:15 -0800

    Day 6 Complete

--- AdventOfCode/2024/Day6.java
+++ AdventOfCode/2024/Day6.java
@@ -13,12 +13,8 @@ public class Day6 {
         Path filePath  = Paths.get("day6input.txt");
         // Path filePath  = Paths.get("day6inputsample.txt");
         List<List<String>> grid = new ArrayList<>();
-        int[] rowKey = {-1,0,1,0};
-        int[] columnKey = {0,1,0,-1} ;
-        int dirKey = 0;
         int posX=0, posY=0;
         Set<String> visited = new HashSet<>();
-        Set<String> visitedWithDir = new HashSet<>();
         int loops =0;
         try {
             List<String> lines = Files.readAllLines(filePath);
@@ -33,42 +29,54 @@ public class Day6 {
         } catch (IOException e) {
             System.out.println("Error reading the file: " + e.getMessage());
         }
+        visited = traverseMap(grid,posX,posY,0,-1,-1);
+        for(String visitedString:visited) {
+            int x = Integer.parseInt(visitedString.substring(0, visitedString.indexOf("x")));
+            int y = Integer.parseInt(visitedString.substring(visitedString.indexOf("x")+1));
+            if(traverseMap(grid, posX, posY, 0, x, y) == null) {
+                loops++;
+            }
+        }
+        // for(int i=0;i<grid.size();i++){
+        //     for(int j=0;j<grid.get(0).size();j++) {
+        //         if(traverseMap(grid, posX, posY, 0, i, j) == null) {
+        //             loops++;
+        //         }
+        //     }
+        // }
+        System.out.println(visited.size());
+        System.out.println(loops);
+    }
+
+    public static Set<String> traverseMap(List<List<String>> grid, int posX, int posY, int dirKey,int tempX, int tempY) {
+        Set<String> visited = new HashSet<>();
+        Set<String> visitedWithDir = new HashSet<>();
         int rowCount = grid.size();
         int columnCount = grid.get(0).size();
+        int[] rowKey = {-1,0,1,0};
+        int[] columnKey = {0,1,0,-1} ;
         String key = posX + "x" + posY;
         String keyWD = posX + "x" + posY+"k"+dirKey;
         visited.add(key);
         visitedWithDir.add(keyWD);
         do {
-            if(grid.get(posX+rowKey[dirKey]).get(posY+columnKey[dirKey]).equals("#")) {
+            if(grid.get(posX+rowKey[dirKey]).get(posY+columnKey[dirKey]).equals("#") || (posX+rowKey[dirKey]  == tempX && posY+columnKey[dirKey] == tempY)) {
                 dirKey++;
                 dirKey = dirKey%4;
-            }
-            if(grid.get(posX+rowKey[dirKey]).get(posY+columnKey[dirKey]).equals(".") || grid.get(posX+rowKey[dirKey]).get(posY+columnKey[dirKey]).equals("^")) {                
+            } else if(grid.get(posX+rowKey[dirKey]).get(posY+columnKey[dirKey]).equals(".") || grid.get(posX+rowKey[dirKey]).get(posY+columnKey[dirKey]).equals("^")) {                
                 posX += rowKey[dirKey];
                 posY += columnKey[dirKey];
                 key = posX + "x" + posY;
                 keyWD = posX + "x" + posY+"k"+dirKey;
-                int probeX = posX;
-                int probeY = posY;
-                while((probeX>=0 && probeX<rowCount) && probeY>=0 && probeY<columnCount) {
-                    probeX += rowKey[(dirKey+1)%4];
-                    probeY += columnKey[(dirKey+1)%4];
-                    String keyWD1 = probeX + "x" + probeY +"k"+(dirKey+1)%4;
-                    if(visitedWithDir.contains(keyWD1)) {
-                        loops++;
-                        // System.out.print(posX+rowKey[dirKey] + " ");
-                        // System.out.println(posY+columnKey[dirKey]);
-                        break;
-                    }
+                if(visitedWithDir.contains(keyWD)) {
+                    return null;
                 }
                 visited.add(key);
                 visitedWithDir.add(keyWD);
+                
             }
             
         } while((posX+rowKey[dirKey]>=0 && posX+rowKey[dirKey]<rowCount) && (posY+columnKey[dirKey])>=0 && posY+columnKey[dirKey]<columnCount);
-
-        System.out.println(visited.size());
-        System.out.println(loops);
+        return visited;
     }
 }
