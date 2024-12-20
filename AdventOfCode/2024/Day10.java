91ff0bb026f7edba61d83a69ac837758dc86961c
Nithish Kolli <nithish.1993@gmail.com>
2024-12-20 00:17:50 -0800

    day 10

--- AdventOfCode/2024/Day10.java
+++ AdventOfCode/2024/Day10.java
@@ -4,10 +4,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class starter {
+public class Day10 {
     public static void main(String[] args) {
-        // Path filePath  = Paths.get("AdventOfCode/2024/day6input.txt");
-        Path filePath  = Paths.get("AdventOfCode/2024/day6inputsample.txt");
+        // Path filePath  = Paths.get("AdventOfCode/2024/day10input.txt");
+        Path filePath  = Paths.get("AdventOfCode/2024/day10inputsample.txt");
         try {
             List<String> lines = Files.readAllLines(filePath);
             for (String line : lines) {
