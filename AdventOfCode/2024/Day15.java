151e4c3ad176409b3de6bcf101b1d308a0c968c1
Nithish Kolli <nithish.1993@gmail.com>
2024-12-22 04:31:42 -0800

    Day 15 part 1

--- AdventOfCode/2024/Day15.java
+++ AdventOfCode/2024/Day15.java
@@ -7,14 +7,14 @@ import java.util.List;
 
 public class Day15 {
     public static void main(String[] args) {
-        // Path filePath  = Paths.get("AdventOfCode/2024/day15input.txt");
-        Path filePath  = Paths.get("AdventOfCode/2024/day15inputsample.txt");
+        Path filePath  = Paths.get("AdventOfCode/2024/day15input.txt");
+        // Path filePath  = Paths.get("AdventOfCode/2024/day15inputsample.txt");
         List<List<Character>> grid = new ArrayList<>();
         int currX=0,currY=0,rowCounter=0;
         try {
             List<String> lines = Files.readAllLines(filePath);
             for (String line : lines) {
-                if (line.charAt(0) != ' ' && line.charAt(0) != '>' && line.charAt(0) != '<' && line.charAt(0) != '^' &&  line.charAt(0) != 'v'){
+                if (line.length()>0 && line.charAt(0) != ' ' && line.charAt(0) != '>' && line.charAt(0) != '<' && line.charAt(0) != '^' &&  line.charAt(0) != 'v'){
                     List<Character> row = new ArrayList<>();
                     int columnCounter=0;
                     for(char ch: line.toCharArray()){
@@ -28,9 +28,26 @@ public class Day15 {
                     rowCounter++;
                     grid.add(row);
                 }
-                else if (line.charAt(0) == '>' || line.charAt(0) == '<' || line.charAt(0) == '^' ||  line.charAt(0) == 'v') {
+                else if (line.length()>0 && (line.charAt(0) == '>' || line.charAt(0) == '<' || line.charAt(0) == '^' ||  line.charAt(0) == 'v')) {
                     for (char ch : line.toCharArray()) {
-                        
+                        if (ch=='>') {
+                            if(moveRobot(grid, currX, currY, 0, 1)){
+                                currY++;
+                            }
+                        } else if (ch=='v') {
+                            if(moveRobot(grid, currX, currY, 1, 0))
+                            {
+                                currX++;
+                            }
+                        } else if (ch=='<') {
+                            if(moveRobot(grid, currX, currY, 0, -1)){
+                                currY--;
+                            }
+                        } else if (ch=='^') {
+                            if(moveRobot(grid, currX, currY, -1, 0)){
+                                currX--;
+                            }
+                        }
                     }
                 }
             }
@@ -38,5 +55,38 @@ public class Day15 {
         } catch (IOException e) {
             System.out.println("Error reading the file: " + e.getMessage());
         }
+        int answer=0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid.get(0).size(); j++) {
+                if(grid.get(i).get(j) == 'O') {
+                    answer += (100*i + j);
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static boolean moveRobot(List<List<Character>> grid, int x, int y,int dirX, int dirY){
+        if(grid.get(x+dirX).get(y+dirY)=='#'){
+            return false;
+        } else if(grid.get(x+dirX).get(y+dirY)=='.'){
+            grid.get(x+dirX).set(y+dirY,'@');
+            grid.get(x).set(y,'.');
+            return true;
+        } else if(grid.get(x+dirX).get(y+dirY)=='O'){
+            int i = 2;
+            while (grid.get(x+(i*dirX)).get(y+(i*dirY))=='O') {
+                i++;
+            }
+            if(grid.get(x+(i*dirX)).get(y+(i*dirY))=='#'){
+                return false;
+            } else if(grid.get(x+(i*dirX)).get(y+(i*dirY))=='.'){
+                grid.get(x+dirX).set(y+dirY,'@');
+                grid.get(x+(i*dirX)).set(y+(i*dirY),'O');
+                grid.get(x).set(y,'.');
+                return true;
+            }
+        }
+        return false;
     }
 }
