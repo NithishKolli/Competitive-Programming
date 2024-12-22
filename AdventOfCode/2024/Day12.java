8a89585e79870a18004debd10a7251cc3b1eff2b
Nithish Kolli <nithish.1993@gmail.com>
2024-12-21 22:08:16 -0800

    Day 12 Incomplete

--- AdventOfCode/2024/Day12.java
+++ AdventOfCode/2024/Day12.java
@@ -13,10 +13,11 @@ import java.util.Set;
 
 public class Day12 {
     public static void main(String[] args) {
-        Path filePath = Paths.get("AdventOfCode/2024/day12input.txt");
-        // Path filePath = Paths.get("AdventOfCode/2024/day12inputsample.txt");
+        // Path filePath = Paths.get("AdventOfCode/2024/day12input.txt");
+        Path filePath = Paths.get("AdventOfCode/2024/day12inputsample.txt");
         List<List<Character>> grid = new ArrayList<>();
         long answer = 0;
+        long answer2 = 0;
         try {
             List<String> lines = Files.readAllLines(filePath);
             for (String line : lines) {
@@ -40,6 +41,15 @@ public class Day12 {
             }
         }
         System.out.println(answer);
+        set = new HashSet<>();
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < columnSize; j++) {
+                if (!set.contains(new AbstractMap.SimpleEntry<>(i, j))) {
+                    answer2 += probeForRegion2(grid, set, i, j);
+                }
+            }
+        }
+        System.out.println(answer2);
     }
 
     private static long probeForRegion(List<List<Character>> grid, Set<Map.Entry<Integer, Integer>> set, int i, int j) {
@@ -63,11 +73,130 @@ public class Day12 {
                     set.add(new AbstractMap.SimpleEntry<>(tx, ty));
                 } else if (!set.contains(new AbstractMap.SimpleEntry<>(tx, ty))) {
                     perimeter++;
-                } else if (set.contains(new AbstractMap.SimpleEntry<>(tx, ty)) && !grid.get(i).get(j).equals(grid.get(tx).get(ty))) {
+                } else if (set.contains(new AbstractMap.SimpleEntry<>(tx, ty))
+                        && !grid.get(i).get(j).equals(grid.get(tx).get(ty))) {
                     perimeter++;
                 }
             }
         }
         return area * perimeter;
+    }
+
+    private static long probeForRegion2(List<List<Character>> grid, Set<Map.Entry<Integer, Integer>> set, int i,
+            int j) {
+        long area = 0;
+        long perimeter = 0;
+        int[] rowKey = { -1, 0, 1, 0 };
+        int[] columnKey = { 0, 1, 0, -1 };
+        int[] incRowKey = { 0, 1, 0, 1 };
+        int[] incColumnKey = { 1, 0, 1, 0 };
+        Set<Map.Entry<Map.Entry<Integer, Integer>, Integer>> dirSet = new HashSet<>();
+        Queue<Map.Entry<Integer, Integer>> queue = new LinkedList<>();
+        queue.add(new AbstractMap.SimpleEntry<>(i, j));
+        set.add(new AbstractMap.SimpleEntry<>(i, j));
+        while (!queue.isEmpty()) {
+            Map.Entry<Integer, Integer> entry = queue.poll();
+            area++;
+            int currX = entry.getKey();
+            int currY = entry.getValue();
+            for (int k = 0; k < 4; k++) {
+                int forwardX = currX + rowKey[k];
+                int forwardY = currY + columnKey[k];
+                int leftX = currX - incRowKey[k];
+                int leftY = currY - incColumnKey[k];
+                int rightX = currX + incRowKey[k];
+                int rightY = currY + incColumnKey[k];
+                if (isOutOfBounds(forwardX, forwardY, grid)
+                        && !set.contains(new AbstractMap.SimpleEntry<>(forwardX, forwardY))
+                        && grid.get(i).get(j).equals(grid.get(forwardX).get(forwardY))) {
+                    queue.add(new AbstractMap.SimpleEntry<>(forwardX, forwardY));
+                    set.add(new AbstractMap.SimpleEntry<>(forwardX, forwardY));
+                } else if (!set.contains(new AbstractMap.SimpleEntry<>(forwardX, forwardY))) {
+                    if (!dirSet.contains(
+                            new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k))
+                            && !dirSet.contains(
+                                    new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(leftX, leftY), k))
+                            && !dirSet.contains(new AbstractMap.SimpleEntry<>(
+                                    new AbstractMap.SimpleEntry<>(rightX, rightY), k))) {
+                        dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k));
+                        perimeter++;
+                        if (isOutOfBounds(rightX, rightY, grid)) {
+                            dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(rightX, rightY), k));
+                        }
+                        if (isOutOfBounds(leftX, leftY, grid)) {
+                            dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(leftX, leftY), k));
+                        }
+                    } else if (dirSet.contains(
+                        new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(leftX, leftY), k))) {
+                            if (!dirSet.contains(
+                                new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k))) {
+                                    dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k));
+                                    perimeter++;
+                                }
+                            
+                            if (isOutOfBounds(rightX, rightY, grid)) {
+                                dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(rightX, rightY), k));
+                            }
+                    } else if (dirSet.contains(new AbstractMap.SimpleEntry<>(
+                        new AbstractMap.SimpleEntry<>(rightX, rightY), k))) {
+                            if (!dirSet.contains(
+                                new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k))) {
+                                    dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k));
+                                    perimeter++;
+                                }
+                            
+                            if (isOutOfBounds(leftX, leftY, grid)) {
+                                dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(leftX, leftY), k));
+                            }
+                    }
+
+                } else if (set.contains(new AbstractMap.SimpleEntry<>(forwardX, forwardY))
+                        && !grid.get(i).get(j).equals(grid.get(forwardX).get(forwardY))) {
+                            if (!dirSet.contains(
+                                new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k))
+                                && !dirSet.contains(
+                                        new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(leftX, leftY), k))
+                                && !dirSet.contains(new AbstractMap.SimpleEntry<>(
+                                        new AbstractMap.SimpleEntry<>(rightX, rightY), k))) {
+                            dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k));
+                            perimeter++;
+                            if (isOutOfBounds(rightX, rightY, grid)) {
+                                dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(rightX, rightY), k));
+                            }
+                            if (isOutOfBounds(leftX, leftY, grid)) {
+                                dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(leftX, leftY), k));
+                            }
+                        } else if (dirSet.contains(
+                            new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(leftX, leftY), k))) {
+                                if (!dirSet.contains(
+                                    new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k))) {
+                                        dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k));
+                                        perimeter++;
+                                    }
+                                
+                                if (isOutOfBounds(rightX, rightY, grid)) {
+                                    dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(rightX, rightY), k));
+                                }
+                        } else if (dirSet.contains(new AbstractMap.SimpleEntry<>(
+                            new AbstractMap.SimpleEntry<>(rightX, rightY), k))) {
+                                if (!dirSet.contains(
+                                    new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k))) {
+                                        dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(currX, currY), k));
+                                        perimeter++;
+                                    }
+                                
+                                if (isOutOfBounds(leftX, leftY, grid)) {
+                                    dirSet.add(new AbstractMap.SimpleEntry<>(new AbstractMap.SimpleEntry<>(leftX, leftY), k));
+                                }
+                        }
+                }
+            }
+        }
+        // System.out.println(grid.get(i).get(j) + " " + area + " " + perimeter + " " + area*perimeter);
+        return area * perimeter;
+    }
+
+    private static boolean isOutOfBounds(int x, int y, List<List<Character>> grid) {
+        return x >= 0 && x < grid.size() && y >= 0 && y < grid.get(0).size();
     }
 }
